9e976c9a5260911e1b4de4feb7c2778b6fa8b98d
Arturo Macias <arturomaciasmx@gmail.com>
2025-03-07 23:18:15 -0800

    adding Clerk auth

--- client/src/components/student/Navbar.tsx
+++ client/src/components/student/Navbar.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
+import { useClerk, UserButton, SignedIn, SignedOut } from "@clerk/clerk-react";
 
 export default function Navbar() {
+  const { openSignIn } = useClerk();
   const isCoursesList = location.pathname.includes("course-list");
+
   return (
     <div
       className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
@@ -14,23 +17,40 @@ export default function Navbar() {
       {/* desktop */}
       <div className="hidden md:flex items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
-          <button>Become educator</button>
-          <Link to={"/my-enrollments"}>My enrollments</Link>
+          <SignedIn>
+            <button>Become educator</button>
+            <Link to={"/my-enrollments"}>My enrollments</Link>
+          </SignedIn>
         </div>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
-          Create account
-        </button>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <button
+            className="bg-blue-600 text-white px-5 py-2 rounded-full"
+            onClick={() => openSignIn()}
+          >
+            Create account
+          </button>
+        </SignedOut>
       </div>
 
       {/* mobile */}
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
-        <div className="flex items-center gap-4">
-          <button>Become educator</button>
-          <Link to={"/my-enrollments"}>My enrollments</Link>
+        <div className="flex items-center gap-2 text-xs">
+          <SignedIn>
+            <button>Become educator</button>
+            <Link to={"/my-enrollments"}>My enrollments</Link>
+          </SignedIn>
         </div>
-        <button>
-          <img src={assets.user_icon} alt="User icon" />
-        </button>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <button onClick={() => openSignIn()}>
+            <img src={assets.user_icon} alt="User icon" />
+          </button>
+        </SignedOut>
       </div>
     </div>
   );
