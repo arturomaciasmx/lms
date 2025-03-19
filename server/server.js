4dbd67a88bbf45d646122a049a13768429ce18a9
Arturo Macias <arturomaciasmx@gmail.com>
2025-03-18 18:02:44 -0700

    project structure

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server runing on port: ${PORT}`);
+});
