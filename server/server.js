ffe2a41bcb5028d5b29d47fd7de191d1b7b51863
Arturo Macias <arturomaciasmx@gmail.com>
2025-03-18 20:13:58 -0700

    connect db and create clerk webhooks

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 const app = express();
+
+// Connect to DB
+await connectDB();
 
 // Middlewares
 app.use(cors());
@@ -11,6 +16,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API working");
 });
+
+app.post("/clerk", express.json(), clerkWebhooks);
 
 const PORT = process.env.PORT || 5001;
 
