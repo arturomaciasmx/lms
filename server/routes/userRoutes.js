149dcaa9fcd2888db8e928d2f47266707711dee6
Arturo Macias <arturomaciasmx@gmail.com>
2025-03-21 12:18:16 -0700

    add rating and progress api endpoints

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserData, getUserEnrolledCourses, purchaseCourse } from "../controllers/userController.js";
+import { addUserRating, getUserCourseProgress, getUserData, getUserEnrolledCourses, purchaseCourse, updateUserCourseProgress } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -9,5 +9,11 @@ userRouter.get("/data", getUserData);
 userRouter.get("/enrolled-courses", getUserEnrolledCourses);
 // purchase
 userRouter.post("/purchase", purchaseCourse);
+// user course progress
+userRouter.post("/update-course-progress", updateUserCourseProgress);
+// get course progress
+userRouter.get("/get-course-progress", getUserCourseProgress);
+// add course rating
+userRouter.get("/add-course-rating", addUserRating);
 
 export default userRouter;
