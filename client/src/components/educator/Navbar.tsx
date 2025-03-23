708ada0f08816afc4b0fa21c4ef75b578bc57906
Arturo Macias <arturomaciasmx@gmail.com>
2025-03-22 22:28:42 -0700

    fix assets calls

--- client/src/components/educator/Navbar.tsx
+++ client/src/components/educator/Navbar.tsx
@@ -1,9 +1,8 @@
 import { UserButton, useUser } from "@clerk/clerk-react";
-import { assets, dummyEducatorData } from "../../assets/assets";
+import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const educatorData = dummyEducatorData;
   const user = useUser();
   return (
     <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3">
